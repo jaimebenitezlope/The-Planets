11c3506e2572c9d61637fb2e465e4adb93ebc71a
Jaime Benitez Lope <bl.jaime@icloud.com>
2024-06-12 10:54:04 +0200

    10:53

---
+++ src/components/navegation/Navegation.jsx
@@ -0,0 +1,14 @@
+import { StyledBox } from './Navegation.style';
+
+const Navegation = () => {
+	return (
+		<>
+			<StyledBox>
+				<p>OVERVIEW</p>
+				<p>STRUCTURE</p>
+				<p>SURFACE</p>
+			</StyledBox>
+		</>
+	);
+};
+export default Navegation;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from '../components/header/Header';
+import Navegation from '../components/header/Header';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<Navegation />
 		</>
 	);
 };
